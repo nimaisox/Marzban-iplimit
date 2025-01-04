53f397aa6e9d73fcfe331ab291639193181408c6
nimaiso <arxisox@gmail.com>
2025-01-04 18:06:08 +0330

    test

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ from utils.read_config import read_config
 from utils.types import PanelType
 
 VERSION = "1.0.8"
-
 parser = argparse.ArgumentParser(description="Help message")
 parser.add_argument("--version", action="version", version=VERSION)
 args = parser.parse_args()
